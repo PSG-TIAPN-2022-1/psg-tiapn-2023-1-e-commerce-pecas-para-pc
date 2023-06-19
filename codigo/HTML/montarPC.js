d74a230b9c068ed04317e9ba3e0b8a3fc3f4076d
rlrosaa <79370543+rlrosaa@users.noreply.github.com>
2023-06-19 15:44:12 -0300

    Criação pagina MontarPC

---
+++ codigo/HTML/montarPC.js
@@ -0,0 +1,6 @@
+function montarPC(){
+    document.getElementById("infoProcessador").innerHTML = document.getElementById("processador").value;
+    document.getElementById("infoPlacaMae").innerHTML = document.getElementById("placa-mae").value;
+    document.getElementById("infoMemoriaRAM").innerHTML = document.getElementById("memoria-ram").value;
+
+}
